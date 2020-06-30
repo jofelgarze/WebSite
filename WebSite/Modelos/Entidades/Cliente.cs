ad3f344a95f0c21049131ba26f88a7a4b04b9079
Josue Garcia Zelaya <jofelgarze@hotmail.com>
2020-06-30 18:19:31 -0500

    Test: Creacion de EF 6

---
+++ WebSite/Modelos/Entidades/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Modelos.Entidades
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "varchar")]
+        [MaxLength(150)]
+        [Required]
+        public String Nombres { get; set; }
+        [Column(TypeName = "varchar")]
+        [MaxLength(150)]
+        [Required]
+        public String Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ WebSite/Modelos/TiendaContextDb.cs
@@ -0,0 +1,40 @@
+﻿namespace WebSite.Modelos
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+    using WebSite.Modelos.Entidades;
+
+    public class TiendaContextDb : DbContext
+    {
+        // El contexto se ha configurado para usar una cadena de conexión 'TiendaContextDb' del archivo 
+        // de configuración de la aplicación (App.config o Web.config). De forma predeterminada, 
+        // esta cadena de conexión tiene como destino la base de datos 'WebSite.Modelos.TiendaContextDb' de la instancia LocalDb. 
+        // 
+        // Si desea tener como destino una base de datos y/o un proveedor de base de datos diferente, 
+        // modifique la cadena de conexión 'TiendaContextDb'  en el archivo de configuración de la aplicación.
+        public TiendaContextDb()
+            : base("name=TiendaContextDb")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Correccion de creacion de tablas
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //Formato de digitos decimales
+            modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            modelBuilder.Conventions.Add(new DecimalPropertyConvention(10, 2));
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
