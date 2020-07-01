2b6a706e7c809a9545aecb699ecd5e43685f0514
Josue Garcia Zelaya <jofelgarze@hotmail.com>
2020-07-01 18:48:57 -0500

    tratando de hacer el grid

---
+++ WebSite/FrmCliente.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="FrmCliente.aspx.cs" Inherits="WebSite.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    <asp:GridView ID="gvClientes" runat="server" ItemType="WebSite.Modelos.Entidades.Cliente" 
+        DataKeyNames="Id" 
+        SelectMethod="consultarClientes" 
+        UpdateMethod="gvClientes_UpdateItem"
+        AutoGenerateColumns="false">
+        <Columns>
+            <asp:DynamicField DataField="Id" />
+            <asp:DynamicField DataField="Nombres" />
+            <asp:DynamicField DataField="Apellidos" />
+            <asp:DynamicField DataField="FechaNacimiento" />
+            <asp:DynamicField DataField="Activo" />        
+        </Columns>    
+    </asp:GridView>
+
+</asp:Content>
---
+++ WebSite/FrmCliente.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebSite.Modelos;
+using WebSite.Modelos.Entidades;
+
+namespace WebSite
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private TiendaContextDb contextDb;
+        private Cliente cliente;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            contextDb = new TiendaContextDb();
+            cliente = contextDb.Clientes.FirstOrDefault();
+        }
+
+        public IQueryable<Cliente> consultarClientes()
+        {
+            var query = contextDb.Clientes;
+            return query;
+        }
+
+        // El nombre de parámetro del id. debe coincidir con el valor DataKeyNames establecido en el control
+        public void gvClientes_UpdateItem(int id)
+        {
+            WebSite.Modelos.Entidades.Cliente item = contextDb.Clientes.Where(c => c.Id == id).SingleOrDefault();
+            // Cargar el elemento aquí, por ejemplo item = MyDataLayer.Find(id);
+            if (item == null)
+            {
+                // No se encontró el elemento
+                ModelState.AddModelError("", String.Format("No se encontró el elemento con id. {0}", id));
+                return;
+            }
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                contextDb.SaveChanges();
+                // Guarde los cambios aquí, por ejemplo MyDataLayer.SaveChanges();
+
+            }
+        }
+    }
+}
