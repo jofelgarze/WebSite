eeb61cfa89b7842e95add7b29cd259bead926c6e
Josue Garcia Zelaya <jofelgarze@hotmail.com>
2020-06-30 20:48:50 -0500

    Correccion de ConnectionString para versiones de EF -6 Se presenta por pantalla datos de la base de datos

--- CapaDatos/TiendaContextoDb.cs
+++ CapaDatos/TiendaContextoDb.cs
@@ -10,7 +10,7 @@
     {
 
         public TiendaContextoDb()
-            : base("name=TiendaContextoDb")
+            : base("TiendaContextoDb")
         {
 
         }
--- WebSite/Default.aspx.cs
+++ WebSite/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaDatos;
+using CapaDatos.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,11 @@ namespace WebSite
 {
     public partial class _Default : Page
     {
+        private TiendaContextoDb contextoDb = new TiendaContextoDb();
+        public Cliente cliente = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            cliente = contextoDb.Clientes.FirstOrDefault();
         }
     }
 }
