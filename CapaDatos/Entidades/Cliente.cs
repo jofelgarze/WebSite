4f416d1523452fedcbb6b9f9f8c8bde02c6af91b
Josue Garcia Zelaya <jofelgarze@hotmail.com>
2020-06-30 19:44:07 -0500

    Se crea capa de datos para ejemplos de entity framework

---
+++ CapaDatos/Entidades/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Entidades
+{
+    [Table("clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Nombres { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Apellidos { get; set; }
+        
+        public DateTime FechaIngreso { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ CapaDatos/TiendaContextoDb.cs
@@ -0,0 +1,30 @@
+﻿namespace CapaDatos
+{
+    using CapaDatos.Entidades;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+
+    public class TiendaContextoDb : DbContext
+    {
+
+        public TiendaContextoDb()
+            : base("name=TiendaContextoDb")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            modelBuilder.Conventions.Add(new DecimalPropertyConvention(10, 2));
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+
+}
